d8cb00a8c9efe4c423790e5887cd361abdfff532
antv <antv@d-soft.com.vn>
2023-09-17 07:25:54 +0700

    Nhan dang khung mat voi 68 diem

--- video_landmarks.py
+++ video_landmarks.py
@@ -1,5 +1,5 @@
 # USAGE
-# python video_landmarks.py --shape_predictor shape_predictor_68_face_landmarks.dat
+# python3 video_landmarks.py --shape_predictor shape_predictor_68_face_landmarks.dat
 from imutils import face_utils
 import argparse
 import cv2 
@@ -10,7 +10,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape_predictor", required=True, help="path to facial landmark predictor")     # dlib’s pre-trained facial landmark detector (phát hiện 68 landmarks)
 args = vars(ap.parse_args())
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)  
 
 # khởi tạo dlib's face detector (HOG-based)
 detector = dlib.get_frontal_face_detector()   # dựa trên HOG + Linear SVM tìm face, xem thêm bài face recognition
@@ -43,14 +43,14 @@ while True:
         # Chuyển dlib's rectange (left, top, right, botttom) = (xmin, ymin, xmax, ymax) to OpenCV-style bounding box (xmin, ymin, w, h)
         # Dễ dàng chuyển được 
         (x, y, w, h) = face_utils.rect_to_bb(rect)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)    # vẽ rectangle quanh khuôn mặt
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)    # vẽ rectangle quanh khuôn mặt
 
         # hiển thị số khuôn mặt trong ảnh, chú ý ở đây đang duyệt qua các detections
         # cv2.putText(frame, "Face #{}".format(i+1), (x-10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
         # duyệt qua các coordinates of facial landmarks (x, y) và vẽ chúng lên ảnh
         for (x, y) in shape:
-            cv2.circle(frame, (x, y), 1, (255, 0, 255), -1)
+            cv2.circle(frame, (x, y), 1, (255, 0, 0), -1)
 
         # hiển thị ảnh đầu ta với face detections + facial landmarks
         cv2.imshow("Output", frame)
